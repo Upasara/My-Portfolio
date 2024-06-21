916d67be96a6e153836a00e9994598ccf0fb8a4b
Upasara <mihirangaupasara2000@gmail.com>
2024-06-21 13:18:12 +0530

    changes to the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,39 +1,38 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assets/3.jpg'
-import HeaderSoc from './HeaderSoc'
-import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import React from 'react';
+import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/3.jpg';
+import HeaderSoc from './HeaderSoc';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 const Header = () => {
+	const [typeEffect] = useTypewriter({
+		words: [
+			'Web Application Developer',
+			'Mobile Application Developer',
+			'ReactJS / NextJS Developer',
+		],
+		loop: {},
+		typeSpeed: 50,
+		deleteSpeed: 40,
+	});
 
-  const [typeEffect] = useTypewriter({
-    words: ['Web Application Developer','Mobile Application Developer', 'Project Manager'],
-    loop:{},
-    typeSpeed: 50,
-    deleteSpeed: 40
-  })
+	return (
+		<header>
+			<div className='container header_container'>
+				<h4>Hello I am</h4>
+				<h1>S.A.Mihiranga Upasara</h1>
+				<h2 className='sub-text'>&nbsp;{typeEffect}</h2>
 
-  return (
-    <header>
-      <div className="container header_container">
-        
-        <h4>Hello I am</h4>
-        <h1>S.A.Mihiranga Upasara</h1>
-        <h2 className="sub-text">&nbsp;{typeEffect}</h2>
-        
-        <CTA/>
-        <HeaderSoc/>
-        
-        <div className="me">
-          <img src={ME} alt="me" className='header_image' />
-        </div>
+				<CTA />
+				<HeaderSoc />
 
-        
+				<div className='me'>
+					<img src={ME} alt='me' className='header_image' />
+				</div>
+			</div>
+		</header>
+	);
+};
 
-      </div>
-    </header>
-  )
-}
-
-export default Header
+export default Header;
